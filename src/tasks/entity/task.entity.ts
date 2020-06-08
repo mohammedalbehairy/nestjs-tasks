690f44eeb950be3018b2f3fe623fc79da8d9df9c
mohammedalbehairy <m.albehairy@deemalab.com>
2020-06-08 08:05:48 +0200

    Create task entity and status enum

--- src/tasks/entity/task.entity.ts
+++ src/tasks/entity/task.entity.ts
@@ -1,1 +1,16 @@
-export class TaskEntity {}
+import { TaskStatus } from './../task-status.enum';
+import { BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: TaskStatus;
+}
